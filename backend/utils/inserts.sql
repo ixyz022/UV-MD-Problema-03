ad205d175ed656193ba5590e6d7cf19c5c3261a3
FelipeLevinir <levinir.f@gmail.com>
2023-06-19 11:28:47 -0400

    trabajando en el crud

--- backend/utils/inserts.sql
+++ backend/utils/inserts.sql
@@ -1,11 +1,6 @@
-
 INSERT INTO `comprador` (`idComprador`, `nombreComprador`) VALUES
 (1, 'Dwayne Johnson'),
 (2, 'Under Taker');
-
-INSERT INTO `Producto` (`numeroVendedor`, `idComprador`, `idTipoProducto`, `precioCompra`) VALUES
-(1, 1, 1, 1000),
-(2, 2, 2, 2000);
 
 INSERT INTO `TipoProducto` (`idTipoProducto`, `descripcionProducto`) VALUES
 (1, 'Cinturón campeonato Lucha Violenta'),
@@ -14,3 +9,7 @@ INSERT INTO `TipoProducto` (`idTipoProducto`, `descripcionProducto`) VALUES
 INSERT INTO `vendedor` (`numeroVendedor`, `nombreVendedor`) VALUES
 (1, 'Cactus Jack'),
 (2, 'Stone Cold Steve Austin');
+
+INSERT INTO `Producto` (`numeroVendedor`, `idComprador`, `idTipoProducto`, `precioCompra`) VALUES
+(1, 1, 1, 1000),
+(2, 2, 2, 2000);
